e6e97691b42bde9e8d18540db7da96e01d842545
Tran Thanh Luong <luong.tran@sutrixsolutions.com>
2020-05-29 17:54:34 +0700

    add contact and pricing page

--- web/app/themes/luonggiacar/index.php
+++ web/app/themes/luonggiacar/index.php
@@ -1,11 +1,19 @@
-<?php get_header(); ?>
-
-    <?php
-        if ( is_404() ) :
-            get_template_part( 'templates/content', '404' );
-        else:
-            get_template_part( 'templates/content', 'page' );
-        endif;
-    ?>
-
-<?php get_footer(); ?>
+<?php if (is_home()) : ?>
+    <?php get_header(); ?>
+        <?php
+            if ( is_404() ) :
+                get_template_part( 'templates/content', '404' );
+            else:
+                get_template_part( 'templates/content', 'page' );
+            endif;
+        ?>
+    <?php get_footer(); ?>
+<?php else:
+    global $wp;
+    $url = home_url( $wp->request );
+    if (basename($url) === 'contact') :
+        get_template_part('templates/content', 'contact');
+    elseif (basename($url) === 'pricing') :
+        get_template_part('templates/content', 'price');
+    endif;
+endif; ?>
