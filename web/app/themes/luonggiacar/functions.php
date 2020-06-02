06bc04d44557b114ba6d520baf4c71917c1b4caf
Tran Thanh Luong <luong.tran@sutrixsolutions.com>
2020-06-02 18:02:39 +0700

    update layout and create widget for about page

--- web/app/themes/luonggiacar/functions.php
+++ web/app/themes/luonggiacar/functions.php
@@ -1,5 +1,17 @@
 <?php
 
-namespace LUONGWP;
-
 require_once 'lib/defines.php';
+require_once 'lib/widgets/about.php';
+
+function sidebar_widgets_init() {
+
+    register_sidebar( array(
+        'name' => 'Dịch vụ của chúng tôi',
+        'id' => 'about_page_child',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="rounded">',
+        'after_title' => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'sidebar_widgets_init' );
--- web/app/themes/luonggiacar/index.php
+++ web/app/themes/luonggiacar/index.php
@@ -1,23 +1,23 @@
-<?php if (is_home()) : ?>
-    <?php get_header(); ?>
-        <?php
-            if ( is_404() ) :
-                get_template_part( 'templates/content', '404' );
-            else:
-                get_template_part( 'templates/content', 'page' );
-            endif;
-        ?>
-    <?php get_footer(); ?>
-<?php else:
-    global $wp;
-    $url = home_url( $wp->request );
-    if (basename($url) === 'contact') :
-        get_template_part('templates/content', 'contact');
-    elseif (basename($url) === 'pricing') :
-        get_template_part('templates/content', 'price');
-    elseif (basename($url) === 'about') :
-        get_template_part('templates/content', 'about');
-    elseif (basename($url) === 'car') :
-        get_template_part('templates/content', 'car');
+<?php
+    if (is_home()) :
+        get_header();
+        get_template_part( 'templates/content', 'page' );
+        get_footer();
+    else:
+        global $wp;
+        $url = home_url( $wp->request );
+        $baseName = basename($url);
+        if ($baseName === 'contact') :
+            get_template_part('templates/content', 'contact');
+        elseif ($baseName === 'pricing') :
+            get_template_part('templates/content', 'price');
+        elseif ($baseName === 'about') :
+            get_template_part('templates/content', 'about');
+        elseif ($baseName === 'car') :
+            get_template_part('templates/content', 'car');
+        else:
+            // 404 page
+            get_template_part( 'templates/content', '404' );
+        endif;
     endif;
-endif; ?>
+    ?>
